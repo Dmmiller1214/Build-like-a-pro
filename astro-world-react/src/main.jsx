2020c5d7b88cf4ad57303bc181f2f20183d3ce2c
Dmmiller1214 <dmmiller1214@gmail.com>
2026-08-10 13:00:16 -0400

    Use Vite base path for routing

--- astro-world-react/src/main.jsx
+++ astro-world-react/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Build-like-a-pro/">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
